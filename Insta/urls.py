3684669eac7095ccb6579ac3ed7d823ba5f261e6
oliverwangx <olivwang@umich.edu>
2020-01-27 20:43:37 -0500

    second

---
+++ Insta/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import include, path
+from Insta.views import (HelloWorld, EditProfile,
+toggleFollow,UserDetailView, addLike, PostDeleteView, addComment,
+PostUpdateView, PostCreateView, PostsView, PostDetailView)
+from . import views
+
+
+urlpatterns = [
+    path('helloworld', HelloWorld.as_view(), name = 'helloworld'),
+    path('', PostsView.as_view(), name = 'posts'),
+    path('posts/<int:pk>', PostDetailView.as_view(), name = 'post_detail'),
+    path('posts/new/', PostCreateView.as_view(), name = 'make_post'),
+    path('posts/update/<int:pk>', PostUpdateView.as_view(), name = 'update_post'),
+    path('posts/delete/<int:pk>', PostDeleteView.as_view(), name = 'delete_post'),
+    path('like', addLike, name='addLike'),
+    path('user/<int:pk>', UserDetailView.as_view(), name = 'user_detail'),
+    path('togglefollow', toggleFollow, name='togglefollow'),
+    path('edit_profile/<int:pk>/', EditProfile.as_view(), name='edit_profile'),
+    path('comment', addComment, name='addComment'),
+]
---
+++ Insta/views.py
@@ -0,0 +1,136 @@
+from annoying.decorators import ajax_request
+from django.views.generic import TemplateView, ListView, DetailView
+from django.shortcuts import render
+from Insta.models import Post, Like, InstaUser, UserConnection, Comment
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from  Insta.forms import CustomUserCreationForm
+
+class HelloWorld(TemplateView):
+    template_name = 'home.html'
+
+# ListView function list of object
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+    # def get_queryset(self):
+    #     current_user = self.request.user
+    #     following = set()
+    #     for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+    #         following.add(conn.following)
+    #     return Post.objects.filter(author__in=following)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class UserDetailView(DetailView):
+    model = InstaUser
+    template_name = 'user_detail.html'
+
+
+class PostCreateView(LoginRequiredMixin , CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+    login_url = 'login'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+    
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts")
+    ## reverse_lazy delete operaion special
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
+
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
+
+
+@ajax_request
+def toggleFollow(request):
+    current_user = InstaUser.objects.get(pk=request.user.pk)
+    follow_user_pk = request.POST.get('follow_user_pk')
+    follow_user = InstaUser.objects.get(pk=follow_user_pk)
+
+    try:
+        if current_user != follow_user:
+            if request.POST.get('type') == 'follow':
+                connection = UserConnection(creator=current_user, following=follow_user)
+                connection.save()
+            elif request.POST.get('type') == 'unfollow':
+                UserConnection.objects.filter(creator=current_user, following=follow_user).delete()
+            result = 1
+        else:
+            result = 0
+    except Exception as e:
+        print(e)
+        result = 0
+
+    return {
+        'result': result,
+        'type': request.POST.get('type'),
+        'follow_user_pk': follow_user_pk
+    }
+
+class EditProfile(UpdateView):
+    model = InstaUser
+    template_name = 'edit_profile.html'
+    fields = ['profile_pic', 'username']
+
+@ajax_request
+def addComment(request):
+    comment_text = request.POST.get('comment_text')
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    commenter_info = {}
+
+    try:
+        comment = Comment(comment=comment_text, user=request.user, post=post)
+        comment.save()
+
+        username = request.user.username
+
+        commenter_info = {
+            'username': username,
+            'comment_text': comment_text
+        }
+
+        result = 1
+    except Exception as e:
+        print(e)
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk,
+        'commenter_info': commenter_info
+    }
